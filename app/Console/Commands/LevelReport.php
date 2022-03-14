0fffda35551083605294049e951ce5f1b7043998
dengj <melinda.dengj@gmail.com>
2022-03-14 10:05:31 +0800

    fix

--- app/Console/Commands/LevelReport.php
+++ app/Console/Commands/LevelReport.php
@@ -2,7 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Models\LevelConfig;
+use App\Models\Sku;
 use App\Models\SkuLevel;
+use App\Models\SpuInfo;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -21,6 +24,11 @@ class LevelReport extends Command
      * @var string
      */
     protected $description = 'sku等级报表';
+
+    /**
+     * @var LevelConfig[]
+     */
+    private static $levelConfigs;
 
     /**
      * Create a new command instance.
@@ -78,6 +86,7 @@ class LevelReport extends Command
                 $model->has_moq = (0 == $productPool->moq) ? 0 : 1;
                 $model->delivery_place = $productPool->name ?? '';
                 $model->arrival_time = $this->getArrivalTime($productPool->supplier_name);
+                $model->product_level = join('、', $this->getLevel($model));
                 $model->save();
             }
 
@@ -113,5 +122,86 @@ class LevelReport extends Command
         }
 
         return 0.0;
+    }
+
+    /**
+     * @param SkuLevel $skuLevel
+     *
+     * @return array
+     */
+    public function getLevel($skuLevel)
+    {
+        $levels = [];
+
+        $sku = Sku::find($skuLevel->sku);
+        if (is_null($sku)) {
+            return $levels;
+        }
+        $spuInfo = SpuInfo::find($sku->spu);
+        if (is_null($spuInfo)) {
+            return $levels;
+        }
+
+        foreach (self::getLevelConfigs() as $config) {
+            if (!is_null($config->arrival_time_min)) {
+                if (1 == $config->arrival_time_min_contain) {
+                    if (bccomp($skuLevel->arrival_time, $config->arrival_time_min, 1) < 0) {
+                        continue;
+                    }
+                } else {
+                    if (bccomp($skuLevel->arrival_time, $config->arrival_time_min, 1) <= 0) {
+                        continue;
+                    }
+                }
+            }
+            if (!is_null($config->arrival_time_max)) {
+                if (1 == $config->arrival_time_max_contain) {
+                    if (bccomp($skuLevel->arrival_time, $config->arrival_time_max, 1) > 0) {
+                        continue;
+                    }
+                } else {
+                    if (bccomp($skuLevel->arrival_time, $config->arrival_time_max, 1) >= 0) {
+                        continue;
+                    }
+                }
+            }
+
+            if (!is_null($config->has_step_price) && $skuLevel->has_step_price != $config->has_step_price) {
+                continue;
+            }
+
+            if (!is_null($config->has_moq) && $skuLevel->has_moq != $config->has_moq) {
+                continue;
+            }
+
+            if (!is_null($config->order_arrange) && $spuInfo->order_arrange !== $config->order_arrange) {
+                continue;
+            }
+
+            if (!is_null($config->is_tort) && $sku->is_tort != $config->is_tort) {
+                continue;
+            }
+
+            if (!empty($config->delivery_place)
+                && !in_array($skuLevel->delivery_place, explode(',', $config->delivery_place))) {
+                continue;
+            }
+
+            $levels[] = $config->level;
+        }
+
+        return $levels;
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|LevelConfig[]
+     */
+    public static function getLevelConfigs()
+    {
+        if (is_null(self::$levelConfigs)) {
+            self::$levelConfigs = LevelConfig::get();
+        }
+
+        return self::$levelConfigs;
     }
 }
---
+++ app/Models/SpuInfo.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SpuInfo extends Model
+{
+    use HasFactory;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'nt_spu_info';
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'spu';
+}
