01e591378eba10b09324fc9dca3d505b91172677
dengj <melinda.dengj@gmail.com>
2022-08-09 16:46:38 +0800

    fix

---
+++ app/Helpers/UpdateBuyPrice.php
@@ -0,0 +1,266 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Models\Product\ReviewLog;
+use App\Models\SkuReview;
+use App\Repositories\ProductPoolRepository;
+use Exception;
+use Illuminate\Support\Facades\DB;
+
+class UpdateBuyPrice extends ReviewAbstract
+{
+    /**
+     * @var SkuReview
+     */
+    private $review;
+
+    /**
+     * @param SkuReview $review
+     */
+    public function __construct($review)
+    {
+        $this->review = $review;
+    }
+
+    public function run()
+    {
+        $instance = new DingApproval();
+        if (!$instance->getProcessInstance($this->review->process_instance_id)) {
+            return;
+        }
+
+        DB::beginTransaction();
+
+        try {
+            $this->review->process_status = $instance->getProcessStatus();
+            $this->review->save();
+
+            $this->reviewLog($instance->getOperationRecords());
+
+            if ($instance->isAgree()) {
+                $this->updateBuyPrice();
+            }
+
+            DB::commit();
+        } catch (Exception $exception) {
+            DB::rollBack();
+        }
+
+        if ($instance->isAgree()) {
+            $this->pushAgreedMessage();
+        } elseif ($instance->isRefuse()) {
+            $this->pushRefusedMessage();
+        }
+    }
+
+    private function pushAgreedMessage()
+    {
+        $message = sprintf(
+            '%s 你好，你在 %s 提交的修改采购价申请已审核通过，请查收。',
+            $this->review->submitter_name,
+            $this->review->create_time
+        );
+        (new DingTalk())->push('修改采购价申请已审核通过', $message, $this->review->submitter_id);
+    }
+
+    private function pushRefusedMessage()
+    {
+        $message = sprintf('%s 你好，你在 %s 提交的修改采购价申请被驳回。', $this->review->submitter_name, $this->review->create_time);
+        (new DingTalk())->push('修改采购价申请被驳回', $message, $this->review->submitter_id);
+    }
+
+    /**
+     * @param object $record
+     */
+    private function devdReview($record)
+    {
+        if (8 != $this->review->status) {
+            return;
+        }
+
+        $this->review->devd_review_time = date('Y-m-d H:i:s', strtotime($record->date));
+        if (self::agreed($record->operation_result)) {
+            $this->review->status = 10;
+            $this->review->save();
+
+            $this->devdPassLog();
+        } elseif (self::refused($record->operation_result)) {
+            $this->review->status = 9;
+            $this->review->devd_reject_reason = $record->remark ?? '';
+            $this->review->save();
+
+            $this->devdRejectLog();
+        }
+    }
+
+    private function devdPassLog()
+    {
+        $log = new ReviewLog();
+        $log->review_id = $this->review->id;
+        $log->action = '审核通过';
+        $log->op_staff_id = $this->review->devd_id;
+        $log->op_staff_name = $this->review->devd_name;
+        $log->op_time = date('Y-m-d H:i:s', strtotime($this->review->devd_review_time));
+        $log->save();
+    }
+
+    private function devdRejectLog()
+    {
+        $log = new ReviewLog();
+        $log->review_id = $this->review->id;
+        $log->action = '驳回';
+        $log->op_staff_id = $this->review->devd_id;
+        $log->op_staff_name = $this->review->devd_name;
+        $log->op_time = date('Y-m-d H:i:s', strtotime($this->review->devd_review_time));
+        $log->save();
+    }
+
+    /**
+     * @param object $record
+     */
+    private function oplReview($record)
+    {
+        if (10 != $this->review->status) {
+            return;
+        }
+
+        $this->review->opl_review_time = date('Y-m-d H:i:s', strtotime($record->date));
+        if (self::agreed($record->operation_result)) {
+            $this->review->status = 12;
+            $this->review->save();
+
+            $this->oplPassLog();
+        } elseif (self::refused($record->operation_result)) {
+            $this->review->status = 11;
+            $this->review->opl_reject_reason = $record->remark ?? '';
+            $this->review->save();
+
+            $this->oplRejectLog();
+        }
+    }
+
+    private function oplPassLog()
+    {
+        $log = new ReviewLog();
+        $log->review_id = $this->review->id;
+        $log->action = '审核通过';
+        $log->op_staff_id = $this->review->opl_id;
+        $log->op_staff_name = $this->review->opl_name;
+        $log->op_time = date('Y-m-d H:i:s', strtotime($this->review->opl_review_time));
+        $log->save();
+    }
+
+    private function oplRejectLog()
+    {
+        $log = new ReviewLog();
+        $log->review_id = $this->review->id;
+        $log->action = '驳回';
+        $log->op_staff_id = $this->review->opl_id;
+        $log->op_staff_name = $this->review->opl_name;
+        $log->op_time = date('Y-m-d H:i:s', strtotime($this->review->opl_review_time));
+        $log->save();
+    }
+
+    /**
+     * @param object $record
+     */
+    private function opdReview($record)
+    {
+        if (12 != $this->review->status) {
+            return;
+        }
+
+        $this->review->opd_review_time = date('Y-m-d H:i:s', strtotime($record->date));
+        if (self::agreed($record->operation_result)) {
+            $this->review->status = 14;
+            $this->review->save();
+
+            $this->opdPassLog();
+        } elseif (self::refused($record->operation_result)) {
+            $this->review->status = 13;
+            $this->review->opd_reject_reason = $record->remark ?? '';
+            $this->review->save();
+
+            $this->opdRejectLog();
+        }
+    }
+
+    private function opdPassLog()
+    {
+        $log = new ReviewLog();
+        $log->review_id = $this->review->id;
+        $log->action = '审核通过';
+        $log->op_staff_id = $this->review->opd_id;
+        $log->op_staff_name = $this->review->opd_name;
+        $log->op_time = date('Y-m-d H:i:s', strtotime($this->review->opd_review_time));
+        $log->save();
+    }
+
+    private function opdRejectLog()
+    {
+        $log = new ReviewLog();
+        $log->review_id = $this->review->id;
+        $log->action = '驳回';
+        $log->op_staff_id = $this->review->opd_id;
+        $log->op_staff_name = $this->review->opd_name;
+        $log->op_time = date('Y-m-d H:i:s', strtotime($this->review->opd_review_time));
+        $log->save();
+    }
+
+    /**
+     * @param array $operationRecords
+     */
+    private function reviewLog($operationRecords)
+    {
+        if (empty($operationRecords)) {
+            return;
+        }
+
+        foreach ($operationRecords as $item) {
+            if (!self::executeTaskNormal($item->operation_type)) {
+                continue;
+            }
+
+            if ($this->review->devd_id == $item->userid) {
+                $this->devdReview($item);
+            } elseif ($this->review->opl_id == $item->userid) {
+                $this->oplReview($item);
+            } elseif ($this->review->opd_id == $item->userid) {
+                $this->opdReview($item);
+            }
+        }
+    }
+
+    private function updateBuyPrice()
+    {
+        $changes = json_decode($this->review->changes, true);
+        if (isset($changes['buy_price'])) {
+            ProductPoolRepository::syncBuyPrice(
+                $this->review->sku,
+                $changes['buy_price'],
+                1,
+                $this->review->submitter_id,
+                $this->review->submitter_name
+            );
+        }
+        if (isset($changes['tax_price'])) {
+            ProductPoolRepository::syncBuyPrice(
+                $this->review->sku,
+                $changes['tax_price'],
+                2,
+                $this->review->submitter_id,
+                $this->review->submitter_name
+            );
+        }
+        if (isset($changes['usd_price'])) {
+            ProductPoolRepository::syncBuyPrice(
+                $this->review->sku,
+                $changes['usd_price'],
+                3,
+                $this->review->submitter_id,
+                $this->review->submitter_name
+            );
+        }
+    }
+}
---
+++ app/Repositories/ProductPoolRepository.php
@@ -0,0 +1,245 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Helpers\DingTalk;
+use App\Models\Product\SkuLog;
+use App\Models\Product\SkuMid;
+use App\Models\Product\SkuStepPrice;
+use App\Models\ProductPool;
+use App\Models\Sku;
+use App\Models\SpuPublished;
+use App\Models\StaffList;
+
+class ProductPoolRepository
+{
+    /**
+     * @param string $sku
+     * @param float  $price
+     * @param int    $priceType
+     * @param string $creatorId
+     * @param string $creatorName
+     */
+    public static function syncBuyPrice($sku, $price, $priceType, $creatorId, $creatorName)
+    {
+        $productPool = ProductPool::find($sku);
+        if (is_null($productPool)) {
+            return;
+        }
+
+        $skuModel = Sku::find($sku);
+        $skuMid = SkuMid::find($productPool->spu);
+        $remark = '';
+
+        switch ($priceType) {
+            case 1:
+                $priceOld = $productPool->buy_price;
+                $productPool->buy_price = $price;
+                $productPool->save();
+                $declaredValue = floor(((($price / 7) * (0.3 * 10)) / 10) * 100) / 100;
+                if (!is_null($skuModel)) {
+                    $skuModel->buy_price = $price;
+                    $skuModel->declared_value = $declaredValue;
+                    $skuModel->save();
+                }
+                if (!is_null($skuMid)) {
+                    $skuMid->buy_price = $price;
+                    $skuMid->declared_value = $declaredValue;
+                    $skuMid->save();
+                }
+                self::updateStepPrice($sku, $price);
+
+                self::pushMessage(
+                    $sku,
+                    ['buy_price' => ['old_str' => $priceOld, 'new_str' => $price]],
+                    $creatorName
+                );
+                $remark = sprintf('非含税RMB：由(%s)修改为(%s);', $priceOld, $price);
+
+                break;
+
+            case 2:
+                $priceOld = $productPool->tax_price;
+                $productPool->tax_price = $price;
+                $productPool->save();
+                if (!is_null($skuModel)) {
+                    $skuModel->tax_price = $price;
+                    $skuModel->save();
+                }
+                if (!is_null($skuMid)) {
+                    $skuMid->tax_price = $price;
+                    $skuMid->save();
+                }
+
+                self::pushMessage(
+                    $sku,
+                    ['tax_price' => ['old_str' => $priceOld, 'new_str' => $price]],
+                    $creatorName
+                );
+                $remark = sprintf('含税RMB：由(%s)修改为(%s);', $priceOld, $price);
+
+                break;
+
+            case 3:
+                $priceOld = $productPool->usd_price;
+                $productPool->usd_price = $price;
+                $productPool->save();
+                if (!is_null($skuModel)) {
+                    $skuModel->usd_price = $price;
+                    $skuModel->save();
+                }
+                if (!is_null($skuMid)) {
+                    $skuMid->usd_price = $price;
+                    $skuMid->save();
+                }
+
+                self::pushMessage(
+                    $sku,
+                    ['usd_price' => ['old_str' => $priceOld, 'new_str' => $price]],
+                    $creatorName
+                );
+                $remark = sprintf('USD：由(%s)修改为(%s);', $priceOld, $price);
+
+                break;
+        }
+
+        if (isset($priceOld)) {
+            $log = new SkuLog();
+            $log->sku = $sku;
+            $log->log_type_id = 38;
+            $log->remark = $remark;
+            $log->create_at = date('Y-m-d H:i:s');
+            $log->create_id = $creatorId;
+            $log->create_name = $creatorName;
+            $log->save();
+        }
+    }
+
+    /**
+     * @param string $sku
+     * @param float  $price
+     */
+    public static function updateStepPrice($sku, $price)
+    {
+        $stepPrice = SkuStepPrice::where('sku', $sku)->where('field_num', 1)->first();
+        if (!is_null($stepPrice)) {
+            $stepPrice->step_price = $price;
+            $stepPrice->save();
+        }
+    }
+
+    public static function pushMessage($sku, $changes, $updaterName = '')
+    {
+        $spuPublishedModels = SpuPublished::where('sku', $sku)->get();
+        if ($spuPublishedModels->isEmpty()) {
+            return;
+        }
+
+        $staffMap = StaffList::whereIn('staff_id', $spuPublishedModels->pluck('shop_user'))
+            ->get()
+            ->keyBy(function ($item) {
+                return $item->staff_id;
+            })
+        ;
+
+        $skuModel = Sku::find($sku);
+
+        $spuPublishedGroup = $spuPublishedModels->reduce(function ($carry, $item) {
+            $shop = $item->shop_name;
+            if ('lazada' == strtolower($item->platform)
+                && preg_match('/lazada-[0-9]+/i', $item->shop_name, $matches)
+                && isset($matches[0])) {
+                $shop = $matches[0];
+            }
+            $carry[$item->shop_user][$shop] = $item;
+
+            return $carry;
+        }, []);
+        foreach ($spuPublishedGroup as $user => $items) {
+            foreach ($items as $shop => $item) {
+                $titles = [];
+                $contents = [];
+                // 运输特性
+                if (isset($changes['arr_trans']) && !empty($changes['arr_trans'])) {
+                    $titles[] = '运输特性';
+                    $contents[] = sprintf(
+                        '运输特性由 %s 修改为 %s ',
+                        $changes['arr_trans']['old_str'] ?? '',
+                        $changes['arr_trans']['new_str'] ?? ''
+                    );
+                }
+                // 产品包装重量
+                if (isset($changes['pack_weight']) && !empty($changes['pack_weight'])) {
+                    $titles[] = '产品包装实重(G)';
+                    $contents[] = sprintf(
+                        '产品包装实重(G)由 %s 修改为 %s ',
+                        $changes['pack_weight']['old_str'] ?? '',
+                        $changes['pack_weight']['new_str'] ?? ''
+                    );
+                }
+                // 非含税RMB
+                if (isset($changes['buy_price']) && !empty($changes['buy_price'])) {
+                    $titles[] = '非含税RMB';
+                    $contents[] = sprintf(
+                        '非含税RMB由 %s 修改为 %s ',
+                        $changes['buy_price']['old_str'] ?? '',
+                        $changes['buy_price']['new_str'] ?? ''
+                    );
+                }
+                // 是否侵权
+                if ('lazada' != strtolower($item->platform)) {
+                    if (isset($changes['is_tort']) && !empty($changes['is_tort'])) {
+                        $titles[] = '侵权状态';
+                        $contents[] = sprintf(
+                            '侵权状态由 %s 修改为 %s ',
+                            $changes['is_tort']['old_str'] ?? '',
+                            $changes['is_tort']['new_str'] ?? ''
+                        );
+                    }
+                }
+                // 销售状态
+                if (isset($changes['sale_state']) && !empty($changes['sale_state'])) {
+                    $titles[] = '销售状态';
+                    $contents[] = sprintf(
+                        '销售状态由 %s 修改为 %s ',
+                        $changes['sale_state']['old_str'] ?? '',
+                        $changes['sale_state']['new_str'] ?? ''
+                    );
+                }
+                // 含税RMB
+                if (isset($changes['tax_price']) && !empty($changes['tax_price'])) {
+                    $titles[] = '含税RMB';
+                    $contents[] = sprintf(
+                        '含税RMB由 %s 修改为 %s ',
+                        $changes['tax_price']['old_str'] ?? '',
+                        $changes['tax_price']['new_str'] ?? ''
+                    );
+                }
+                // USD
+                if (isset($changes['usd_price']) && !empty($changes['usd_price'])) {
+                    $titles[] = 'USD';
+                    $contents[] = sprintf(
+                        'USD由 %s 修改为 %s ',
+                        $changes['usd_price']['old_str'] ?? '',
+                        $changes['usd_price']['new_str'] ?? ''
+                    );
+                }
+
+                if (!empty($titles) && !empty($contents)) {
+                    $message = sprintf(
+                        '尊敬的：%s\n\n亲，您在 %s 上架至 %s 的SKU：%s\n\n品名：%s\n\n%s\n\n更新时间：%s\n\n修改人：%s\n\n请知悉！',
+                        $staffMap[$user]->staff_name ?? '',
+                        $item->publish_time,
+                        $shop,
+                        $item->sku,
+                        $skuModel->sku_name ?? '',
+                        join('\n\n', $contents),
+                        date('Y-m-d H:i:s'),
+                        $updaterName
+                    );
+                    (new DingTalk())->push(join('|', $titles).'变更通知', $message, $user);
+                }
+            }
+        }
+    }
+}
