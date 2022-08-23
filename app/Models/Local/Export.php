cd46b72925dd41db2c7b0df6a68b3575b7b6ee42
dengj <melinda.dengj@gmail.com>
2022-08-23 14:04:12 +0800

    fix

---
+++ app/Models/Local/Export.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models\Local;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Export extends Model
+{
+    use HasFactory;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The connection name for the model.
+     *
+     * @var null|string
+     */
+    protected $connection = 'local';
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'export';
+}
--- config/database.php
+++ config/database.php
@@ -79,6 +79,25 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        'local' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST_LOCAL', '127.0.0.1'),
+            'port' => env('DB_PORT_LOCAL', '3306'),
+            'database' => env('DB_DATABASE_LOCAL', 'forge'),
+            'username' => env('DB_USERNAME_LOCAL', 'forge'),
+            'password' => env('DB_PASSWORD_LOCAL', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
         // 测评系统
         'assess' => [
             'driver' => 'mysql',
