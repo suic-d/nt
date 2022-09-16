6434a8cc62b983df1c2d9a1db580ccd73e189dde
dengj <melinda.dengj@gmail.com>
2022-09-16 14:57:09 +0800

    fix

---
+++ app/Console/Commands/Product/SyncDeptUser.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands\Product;
+
+use App\Models\DeptList;
+use GuzzleHttp\Client;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Console\Command;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+class SyncDeptUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'product:syncDeptUser';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '获取部门下用户';
+
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @var Logger
+     */
+    protected $logger;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->client = new Client(['base_uri' => env('BASE_URL'), 'verify' => false]);
+        $this->logger = new Logger('syncDeptUser');
+        $this->logger->pushHandler(new StreamHandler(storage_path('logs/syncDeptUser'), Logger::INFO));
+    }
+
+    public function handle()
+    {
+        $requests = function () {
+            $deptIdArr = DeptList::get(['dept_id'])->pluck('dept_id');
+            foreach ($deptIdArr as $value) {
+                yield $value => new Request('GET', 'index.php/oaapi/oaapi/deptUser?id='.$value);
+            }
+        };
+        $pool = new Pool($this->client, $requests(), [
+            'concurrency' => 5,
+            'fulfilled' => function ($response) {
+                $this->logger->info($response->getBody()->getContents());
+            },
+            'rejected' => function ($reason) {
+                $this->logger->error($reason->getMessage());
+            },
+        ]);
+        $pool->promise()->wait();
+    }
+}
